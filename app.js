7bcc8faf3b6449f4ab01a4931a0c4bee72624075
mannyabeb <64314295+mannyabeb@users.noreply.github.com>
2021-07-23 14:30:09 +0300

    forth commit

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-// Function expression to select elements `
 
-const selectElement = (s) => document.querySelector(s);
-const navLinks = document.querySelectorAll(".nav-link");
-
-selectElement(".burger-menu-icon").addEventListener("click", () => {
-    selectElement(".nav-list").classList.toggle("active");
-    selectElement(".burger-menu-icon").classList.toggle("toggle")
-
-    navLinks.forEach((link, index) => {
-        if (link.style.animation){
-            link.style.animation = ""
-        }else{
-            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-            console.log(index/7 + 0.5)
-        }
-    })
-});
+  var functionRun=false;
+  function menu(){
+    var elem=document.getElementById("menu");
+    if(elem.className === "header"){
+      elem.className += "responsive";
+      elem.style.top="0";
+    }
+    else{
+      elem.className = "navb";
+      elem.style.top="-50%";
+    }
+  }
